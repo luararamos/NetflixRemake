bf149e27233712f4574de7271cec1d1751d8847f
luaraRamos <luara.m.ramos@gmail.com>
2023-04-10 14:07:34 -0300

    mvc- models(category, movie)

--- app/src/main/java/co/tiagoaguiar/netflixremake/MainActivity.kt
+++ app/src/main/java/co/tiagoaguiar/netflixremake/MainActivity.kt
@@ -4,17 +4,26 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import co.tiagoaguiar.netflixremake.model.Movie
 
 class MainActivity : AppCompatActivity() {
+
+    // m-v-c (model - [view/controller] activity)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val adapter = MainAdapter()
+        val movies = mutableListOf<Movie>()
+        for(i in 0 until 60){
+            val movie = Movie("htps://exemplo.com/$i.jpg")
+            movies.add(movie)
+
+        }
+
+        val adapter = MainAdapter(movies)
         val rv: RecyclerView = findViewById(R.id.rv_main)
         rv.layoutManager = LinearLayoutManager(this)
         rv.layoutManager = LinearLayoutManager(this)
         rv.adapter = adapter
     }
-
 }
