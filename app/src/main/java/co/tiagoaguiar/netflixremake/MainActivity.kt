37a5c16e746a8e5befdbe096fa283360b7f09f93
luaraRamos <luara.m.ramos@gmail.com>
2023-04-27 09:56:43 -0300

    for in json

--- app/src/main/java/co/tiagoaguiar/netflixremake/MainActivity.kt
+++ app/src/main/java/co/tiagoaguiar/netflixremake/MainActivity.kt
@@ -1,6 +1,7 @@
 package co.tiagoaguiar.netflixremake
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -8,7 +9,7 @@ import co.tiagoaguiar.netflixremake.model.Category
 import co.tiagoaguiar.netflixremake.model.Movie
 import co.tiagoaguiar.netflixremake.util.CategoryTask
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CategoryTask.Callback {
 
     // m-v-c (model - [view/controller] activity)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,6 +24,12 @@ class MainActivity : AppCompatActivity() {
         rv.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
         rv.adapter = adapter
 
-        CategoryTask().execute("https://api.tiagoaguiar.co/netflixapp/home?apiKey=f47cbd00-e981-4b94-a66f-8f8ee06e8314")
+        CategoryTask(this).execute("https://api.tiagoaguiar.co/netflixapp/home?apiKey=f47cbd00-e981-4b94-a66f-8f8ee06e8314")
+    }
+
+    override fun onResult(categories: List<Category>) {
+        //  aqui será quando o CategoryTask chamará de volta
+        //  (callback) - listener
+        Log.i("Teste MainActivity", categories.toString())
     }
 }
--- app/src/main/java/co/tiagoaguiar/netflixremake/util/CategoryTask.kt
+++ app/src/main/java/co/tiagoaguiar/netflixremake/util/CategoryTask.kt
@@ -1,6 +1,9 @@
 package co.tiagoaguiar.netflixremake.util
 
 import android.util.Log
+import co.tiagoaguiar.netflixremake.model.Category
+import co.tiagoaguiar.netflixremake.model.Movie
+import org.json.JSONObject
 import java.io.BufferedInputStream
 import java.io.ByteArrayOutputStream
 import java.io.IOException
@@ -9,44 +12,83 @@ import java.net.URL
 import java.util.concurrent.Executors
 import javax.net.ssl.HttpsURLConnection
 
-class CategoryTask {
+class CategoryTask(private val callback: Callback) {
+
+    interface Callback {
+        fun onResult(categories: List<Category>)
+
+    }
+
     fun execute(url: String) {
         // nesse momento, estamos utilizando a UI-thread (1)
         val executor = Executors.newSingleThreadExecutor()
+
         executor.execute {
+            var urlConnection: HttpsURLConnection? = null
+            var buffer: BufferedInputStream? = null
+            var stream: InputStream? = null
             // nesse momento, estamos utilizando a NOVA-thread [processo paralelo] (2)
             //>>>>>>>AQUI é um código meio que PADRAO<<<<<<
             try {
-                val requestURL = URL(url) // abrir uma URL
-                val urlConnection =
-                    requestURL.openConnection() as HttpsURLConnection //abrir a conexão
+                val requestURL = URL(url)  // abrir uma URL
+                urlConnection = requestURL.openConnection() as HttpsURLConnection  //abrir a conexão
                 urlConnection.readTimeout = 2000 //tempo leitura (2s)
                 urlConnection.connectTimeout = 2000 // tempo conexão (2s)
 
-                val statusCode: Int = urlConnection.responseCode //statusCode 202/404...
+                val statusCode: Int = urlConnection.responseCode
                 if (statusCode > 400) {
-                    throw IOException("Erro na comunicação com o servidor")
+                    throw IOException("Erro na comunicação com o servidor!")
                 }
 
-                val stream = urlConnection.inputStream // sequencia bytes
+                stream = urlConnection.inputStream // sequencia bytes
 
                 // forma1: simples e rápida
                 // val jsonAsString = stream.bufferedReader().use { it.readText() } // bytes -> String
 
                 // forma2: bytes -> String
-                val buffer = BufferedInputStream(stream)
+                buffer = BufferedInputStream(stream)
                 val jsonAsString = toString(buffer)
 
                 // o JSON está preparado para ser convertido em um DATA CLASS
-                Log.i("Teste", jsonAsString)
+                val categories = toCategories(jsonAsString)
+                callback.onResult(categories)
 
             } catch (e: IOException) {
                 Log.e("Teste", e.message ?: "erro desconhecido", e)
-
+            } finally {
+                urlConnection?.disconnect()
+                stream?.close()
+                buffer?.close()
             }
             //>>>>>>>AQUI é um código meio que PADRAO<<<<<<
 
         }
+    }
+
+    private fun toCategories(jsonAsString: String) : List<Category> {
+        val categories = mutableListOf<Category>()
+
+        val jsonRoot = JSONObject(jsonAsString)
+        val jsonCategories = jsonRoot.getJSONArray("category")
+        for (i in 0 until jsonCategories.length()) {
+            val jsonCategory = jsonCategories.getJSONObject(i)
+
+            val title = jsonCategory.getString("title")
+            val jsonMovies = jsonCategory.getJSONArray("movie")
+
+            val movies = mutableListOf<Movie>()
+            for (j in 0 until jsonMovies.length()) {
+                val jsonMovie = jsonMovies.getJSONObject(j)
+                val id = jsonMovie.getInt("id")
+                val coverUrl = jsonMovie.getString("cover_url")
+
+                movies.add(Movie(id, coverUrl))
+            }
+
+            categories.add(Category(title, movies))
+        }
+
+        return categories
     }
 
     private fun toString(stream: InputStream): String {
